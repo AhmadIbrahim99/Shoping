4211b380b31392b100b1a46ea88bc99ce0711f1e
Ahmed <ahmednafeth2020@gmail.com>
2020-12-28 22:49:54 +0200

    @Test Ahmed shahwan

--- app/src/test/java/com/example/khaled/settings.java
+++ app/src/test/java/com/example/khaled/settings.java
@@ -5,11 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class settings {
+
    SettinsActivity settinsActivity= new SettinsActivity();
    //Ahmed shahwan
 
     @Test
-    public void test()throws NullPointerException{//التحقق عند تغيير البيانات
+    public void test1()throws NullPointerException{
+        //التحقق عند تغيير البيانات
+
         assertTrue(settinsActivity.checker.toString(),true);
 
     }
