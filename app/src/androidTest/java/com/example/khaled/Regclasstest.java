37df5ab4627e44ce294dc395162cb57c17e31516
AhmadIbrahim99 <ibrahim.s.1999@hotmail.com>
2020-12-28 20:00:37 +0200

    Register TEst

---
+++ app/src/androidTest/java/com/example/khaled/Regclasstest.java
@@ -0,0 +1,30 @@
+package com.example.khaled;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+public class Regclasstest {
+    @Rule
+
+    public ActivityTestRule<RegisterActivity> testRule =
+            new ActivityTestRule<RegisterActivity>(RegisterActivity.class);
+
+    @Test
+    public void user_can_enter_Name(){
+        onView(withId(R.id.register_username_input)).perform(typeText("Ahmad"));
+    }
+    @Test
+    public void user_can_enter_phone_Number(){
+        onView(withId(R.id.register_phone_number_input)).perform(typeText("0597674243"));
+    }
+    @Test
+    public void user_can_enter_Password(){
+        onView(withId(R.id.register_password_input)).perform(typeText("Ahmad"));
+    }
+}
