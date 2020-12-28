45e8a507c3790fad5f2e8e5c9741f6ec01f9d2cc
AhmadIbrahim99 <ibrahim.s.1999@hotmail.com>
2020-12-28 18:16:58 +0200

    new

---
+++ app/src/main/java/com/example/khaled/Admin/AdminNewOrdersActivity.java
@@ -0,0 +1,16 @@
+package com.example.khaled.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.khaled.R;
+
+public class AdminNewOrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_new_orders);
+    }
+}
---
+++ app/src/main/java/com/example/khaled/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.khaled.Prevalent;
+
+import com.example.khaled.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
